48af248f22f5b88cd4965f129e494c1b085a311a
SaltedFish <1054510164@qq.com>
2023-12-05 13:39:16 +0800

    refactor: extract fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
-// eslint-disable-next-line camelcase
-import { Inter, Space_Grotesk } from 'next/font/google';
 import type React from 'react';
+import { inter, spaceGrotesk } from './font';
 import './globals.css';
 
 export const metadata:Metadata = {
@@ -13,17 +12,6 @@ export const metadata:Metadata = {
   }
 };
 
-const inter = Inter({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-inter'
-});
-
-const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-spaceGrotesk'
-});
 export default function RootLayout ({
   children
 }: {
