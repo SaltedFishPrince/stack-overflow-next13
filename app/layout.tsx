b64bfb530faaa25e9e0efc8b7467496fab67457f
SaltedFish <1054510164@qq.com>
2023-12-05 11:41:54 +0800

    add layout and assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,29 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import React from 'react'
+import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from 'next/font/google';
+import type React from 'react';
+import './globals.css';
 
+export const metadata:Metadata = {
+  title: 'FishFlow',
+  description: 'A fish flow for your fish',
+  icons: {
+    icon: '/public/assets/images/site-logo.svg'
+  }
+};
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+});
 export default function RootLayout ({
   children
 }: {
@@ -9,8 +32,11 @@ export default function RootLayout ({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className='h1-bold'>fish</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
