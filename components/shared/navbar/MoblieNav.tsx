9e86488347c45a929de8a23b18c97aa532421b04
SaltedFish <1054510164@qq.com>
2023-12-06 18:17:06 +0800

    feat(nav/sidebar): restore before render

--- components/shared/navbar/MoblieNav.tsx
+++ components/shared/navbar/MoblieNav.tsx
@@ -1,4 +1,4 @@
-import SideBarList from '@/components/shared/sidebar-list';
+'use client';
 import { Button } from '@/components/ui/button';
 import {
   Sheet,
@@ -8,9 +8,47 @@ import {
   SheetTitle,
   SheetTrigger
 } from '@/components/ui/sheet';
+import { sidebarLinks } from '@/constants';
 import { SignedOut } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const NavContent = () => {
+  const pathname = usePathname();
+  const isCurrentRoute = (route:string) => (pathname.includes(route) && route.length > 1) ||
+  pathname === route;
+  return (
+    <section className="flex h-full flex-col gap-6 pt-16">
+      {sidebarLinks.map((item) => {
+        const isActive = isCurrentRoute(item.route);
+        return (
+          <SheetClose asChild key={item.route}>
+            <Link
+              href={item.route}
+              className={`${
+                isActive
+                  ? 'primary-gradient rounded-lg text-light-900'
+                  : 'text-dark300_light900'
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? '' : 'invert-colors'}`}
+              />
+              <p className={`${isActive ? 'base-bold' : 'base-medium'}`}>
+                {item.label}
+              </p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
+};
 
 const MoblieNav = () => {
   return (
@@ -43,10 +81,7 @@ const MoblieNav = () => {
         </SheetHeader>
         <div>
           <SheetClose>
-            <SideBarList
-              as='section'
-              className='flex h-full flex-col gap-6 pt-16'
-              itemOuter={<SheetClose asChild />}/>
+            <NavContent/>
           </SheetClose>
           <SignedOut>
             <div className="flex flex-col gap-3">
