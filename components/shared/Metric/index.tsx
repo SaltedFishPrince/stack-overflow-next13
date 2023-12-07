6114047eef063b5123b7256f243144808a99bf14
SaltedFish <1054510164@qq.com>
2023-12-07 17:28:37 +0800

    feat(comp): add Metric component

---
+++ components/shared/Metric/index.tsx
@@ -0,0 +1,74 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface MetricProps {
+  imgUrl: string;
+  alt: string;
+  value: string | number;
+  title: string;
+  href?: string;
+  textStyles?: string;
+  isAuthor?: boolean;
+}
+
+const MetricContent = ({
+  imgUrl,
+  alt,
+  value,
+  title,
+  href,
+  isAuthor,
+  textStyles
+}: MetricProps) => {
+  return (
+    <>
+      <Image
+        src={imgUrl}
+        width={16}
+        height={16}
+        alt={alt}
+        className={`object-contain ${href ? 'rounded-full' : ''}`}
+      />
+      <p className={`${textStyles} flex items-center gap-1`}>
+        {value}
+        <span
+          className={`small-regular line-clamp-1 ${
+            isAuthor ? 'max-sm:hidden' : ''
+          }`}
+        >
+          {title}
+        </span>
+      </p>
+    </>
+  );
+};
+
+const Metric = ({
+  imgUrl,
+  alt,
+  value,
+  title,
+  href,
+  isAuthor,
+  textStyles
+}: MetricProps) => {
+  const metricContent = (
+    <MetricContent
+      imgUrl={imgUrl}
+      alt={alt}
+      value={value}
+      title={title}
+      isAuthor={isAuthor}
+      textStyles={textStyles}
+    />);
+  if (href) {
+    return (
+      <Link href={href} className="flex-center gap-1">
+        {metricContent}
+      </Link>
+    );
+  }
+  return <div className="flex-center flex-wrap gap-1">{metricContent}</div>;
+};
+
+export default Metric;
