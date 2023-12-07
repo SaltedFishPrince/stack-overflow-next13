846adfb7acf80cf4ac79cd0f4c3b9250c96ce89c
SaltedFish <1054510164@qq.com>
2023-12-07 16:20:10 +0800

    feat(home):add home-filter and select-filter

---
+++ components/home/HomeFilter.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React from 'react';
+
+import { HomePageFilters } from '@/constants/filters';
+import { Button } from '../ui/button';
+
+const HomeFilters = () => {
+  const [active, setActive] = React.useState<null | string>(null);
+  return (
+    <div className="mt-10 hidden flex-wrap gap-3 md:flex">
+      {HomePageFilters.map(({ name, value }) => (
+        <Button
+          key={value}
+          onClick={() => { setActive(value); }}
+          className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
+            active === value
+              ? 'bg-primary-100 text-primary-500'
+              : 'bg-light-800 text-light-500 hover:bg-light-700 dark:bg-dark-300 dark:hover:bg-dark-400'
+          }`}
+        >
+          {name}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default HomeFilters;
