c28e5bcc40d8e60875dc52adeb068213bf966982
SaltedFish <1054510164@qq.com>
2023-12-18 13:56:04 +0800

    refactor: fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
 import type React from 'react';
 
 type Props = {
-  children:React.ReactNode
+  children: React.ReactNode
 };
-const Layout = ({ children }:Props) => {
+const Layout = ({ children }: Props) => {
   return (
     <div className='flex-center min-h-screen w-full'>
       {children}
