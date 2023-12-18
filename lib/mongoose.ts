350a6d1dc7e727e7feee144f1c90fad0afdda29e
SaltedFish <1054510164@qq.com>
2023-12-18 14:34:18 +0800

    refactor: fix

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -3,12 +3,19 @@ let isConnect: boolean = false
 export const connectToDatabase = async () => {
   // 开启严格查询
   mongoose.set('strictQuery', true)
+
+  if (!process.env.MONGODB_URL) {
+    return console.log("MISSING MONGODB_URL");
+  }
+
   if (isConnect) {
     return console.log('database connected')
   }
 
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/devflow')
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "dev_flow",
+    });
     isConnect = true
     console.log('database connected success')
   } catch (error) {
