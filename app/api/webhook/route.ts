22910a4b39b6872f049d9e2ca828a10f4ff178c3
SaltedFish <1054510164@qq.com>
2023-12-18 15:42:22 +0800

    refactor: fix

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -8,7 +8,6 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
-
   if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
@@ -69,7 +68,7 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ message: "OK", user: mongoUser });
   }
-
+  console.log({ eventType })
   // 更新用户
   if (eventType === "user.updated") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
