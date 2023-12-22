150207206cff6246ce8bcac9a51e4e4df61f876d
SaltedFish <1054510164@qq.com>
2023-12-22 14:27:57 +0800

    fix

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -4,6 +4,7 @@ import { useTheme } from '@/context/theme-provider';
 import useIsServer from '@/hooks/useIsServer';
 import Image from 'next/image';
 import React from 'react';
+
 const SunIcon = () => {
   return (
     <Image
